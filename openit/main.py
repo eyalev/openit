bdb53a7e755d28b05eeadf01c11288630c4e3b4d
Eyal Levin <eyalev@gmail.com>
2020-04-30 23:22:09 +0300

    Update

--- openit/main.py
+++ openit/main.py
@@ -10,21 +10,20 @@ current_dir_path = os.path.dirname(__file__)
 
 
 @click.command()
-@click.argument('arg')
+@click.argument("arg")
 def main(arg):
 
     config_dict = _get_config_dict()
-    sites_dict = config_dict['sites']
+    sites_dict = config_dict["sites"]
     if arg in sites_dict:
         url = sites_dict[arg]
-        command = f'google-chrome --new-window {url}'
+        command = f"google-chrome --new-window {url}"
         subprocess.call(shlex.split(command))
         # print(command)
     else:
-        print('No match')
+        print("No match")
 
 
 def _get_config_dict():
-    config_dict = P(f'{current_dir_path}/config.yaml').to_dict()
+    config_dict = P(f"{current_dir_path}/config.yaml").to_dict()
     return config_dict
-
